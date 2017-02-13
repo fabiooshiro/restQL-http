b04c130c324018ffc7cd6b40cb6ee9bc5aed3c66
Jeferson Leao <jeferson.leao@b2wdigital.com>
2017-02-13 13:44:33 -0200

    updating restql core version

--- project.clj
+++ project.clj
@@ -17,7 +17,7 @@
                  [ring/ring-json "0.4.0"]
                  [org.clojure/java.classpath "0.2.3"]
                  [com.novemberain/monger "3.0.2"]
-                 [com.b2wdigital/restql-core "0.4.0"]
+                 [com.b2wdigital/restql-core "0.4.1"]
                  [commons-validator "1.5.1"]]
   :plugins [[lein-expectations "0.0.8"]
             [lein-autoexpect "1.4.0"]
