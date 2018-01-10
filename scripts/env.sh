501e98c531b413a813159005b954f94de15bf012
Ricardo Mayerhofer <ricardo.mayerhofer@ideais.com.br>
2018-01-10 20:38:55 -0200

    Optional MongoDB configuration.

--- scripts/env.sh
+++ scripts/env.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 export PORT=9000
 
-export MONGO_URL=mongodb://localhost:27017/restql-server
+# Uncomment if you'll use saved queries and saved MongoDB endpoints
+#export MONGO_URL=mongodb://localhost:27017/restql-server
+
 export PLANETS=https://swapi.co/api/planets/:id
