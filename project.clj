e3d8f0a26a4438ed485c954afef888c636c77d6c
Lucas Barros <lucsbarros@gmail.com>
2018-11-07 20:49:13 -0200

    chore(core): update to version 2.6.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :license {:name "Eclipse Public License"
             :url  "http://restql.b2w.io"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [b2wdigital/restql-core "2.5.3"]
+                 [b2wdigital/restql-core "2.6.1-SNAPSHOT"]
                  [aleph "0.4.6"]
                  [compojure "1.6.1"]
                  [environ "1.0.0"]
