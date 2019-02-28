4b715be6ade43f66eaccee88a2d7ecd5691aecdc
Lucas Barros <lucsbarros@gmail.com>
2019-02-28 10:23:20 -0300

    bump version to 2.8.14

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject restql-http "v2.8.13" :description "restQL HTTP"
+(defproject restql-http "v2.8.14" :description "restQL HTTP"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url  "http://restql.b2w.io"}
