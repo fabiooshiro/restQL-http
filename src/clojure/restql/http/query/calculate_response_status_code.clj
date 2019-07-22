0ed8572aaf235e49548a5cef90e0f3af5d80a4d1
Lucas Barros <lucsbarros@gmail.com>
2019-07-22 20:04:01 -0300

    fix(query): add resource details list support (#144)

--- src/clojure/restql/http/query/calculate_response_status_code.clj
+++ src/clojure/restql/http/query/calculate_response_status_code.clj
@@ -1,34 +1,44 @@
 (ns restql.http.query.calculate-response-status-code)
 
-(defn- should-ignore-errors
-  "Searches for an ignore-errors meta on a given item
-   and returns it's value"
-  [item]
-
-  (-> item
+(defn- not-error-ignored? [resource-result]
+  (-> resource-result
       :details
       :metadata
       :ignore-errors
-      (= "ignore")))
+      (= "ignore")
+      (not)))
 
-(defn- higher-value
-  "Returns the max between a and b"
-  [a b]
+(defn- not-error-ignoreds [resource-results]
+  (filter not-error-ignored? resource-results))
 
-  (cond
-    (nil? a) b
-    (nil? b) a
-    (> a b) a
-    :else b))
+(defn- extract-status [details]
+  (cond (sequential? details) (map extract-status details)
+        :else (conj [] (:status details))))
+
+(defn- only-resource-status [resource-result]
+  (->> resource-result
+       (map :details)
+       (map extract-status)
+       (flatten)))
+
+(defn- fix-status [statuses]
+  (replace {nil 500 0 503 204 200} statuses))
+
+(defn- get-max [a b]
+  (cond (nil? a) b
+        (nil? b) a
+        (> a b) a
+        :else b))
+
+(defn- higher-value [statuses]
+  (reduce get-max 200 statuses))
 
 (defn calculate-response-status-code
   "Calculates the response status code of a given result"
   [result]
-
   (->> result
        (vals)
-       (flatten)
-       (filter (complement should-ignore-errors))
-       (map (comp :status :details))
-       (replace {0 503 204 200})
-       (reduce higher-value 200)))
+       (not-error-ignoreds)
+       (only-resource-status)
+       (fix-status)
+       (higher-value)))
--- test/clojure/restql/http/query/status_code_test.clj
+++ test/clojure/restql/http/query/status_code_test.clj
@@ -3,43 +3,53 @@
             [restql.http.query.calculate-response-status-code :as status-code]))
 
 (deftest ignoring-errors-when-flagged
-  
-  (let [should-ignore-errors #'status-code/should-ignore-errors]
+  (let [not-error-ignored? #'status-code/not-error-ignored?]
     (testing "Should return true for ignore"
-      (is (true? (should-ignore-errors {:details {:metadata {:ignore-errors "ignore"}}}))))
-
+      (is (false? (not-error-ignored? {:details {:metadata {:ignore-errors "ignore"}}}))))
     (testing "Should return false if no metadata provided"
-      (is (false? (should-ignore-errors {:details {}}))))))
-
+      (is (true? (not-error-ignored? {:details {}}))))))
 
 (deftest returning-higher-value
   (let [higher-value #'status-code/higher-value]
     (testing "Should return highers"
-      (is (=
-           [503 200 404 422 200 408]
-           [(higher-value 503 200)
-            (higher-value 200 200)
-            (higher-value 404 301)
-            (higher-value 200 422)
-            (higher-value nil 200)
-            (higher-value 408 nil)])))))
+      (is (= 204 (higher-value [204 nil])))
+      (is (= 408 (higher-value [200 204 nil 408])))
+      (is (= 503 (higher-value [503 200 404 422 200 408]))))))
+
+(deftest returning-fix-status
+  (let [fix-status #'status-code/fix-status]
+    (testing "Should return highers"
+      (is (= [200 500] (fix-status [204 nil])))
+      (is (= [200 200 500 408] (fix-status [200 204 nil 408])))
+      (is (= [503 200 404 422 200 408] (fix-status [503 200 404 422 200 408]))))))
 
 (deftest calculating-response-status
   (let [calculate-response-status-code #'status-code/calculate-response-status-code]
     (testing "Should return higher status "
-      (is (=
-           404
-           (calculate-response-status-code {:hero {:details {:status 200}}
-                                            :villain {:details {:status 404}}}))))
-
+      (is (= 404
+             (calculate-response-status-code {:hero {:details {:status 200}}
+                                              :villain {:details {:status 404}}}))))
+    (testing "Should return higher status "
+      (is (= 408
+             (calculate-response-status-code {:hero {:details {:status 200}}
+                                              :villain {:details [{:status 408}]}}))))
+    (testing "Should return higher status"
+      (is (= 408
+             (calculate-response-status-code {:hero {:details {:status 200}}
+                                              :villain {:details [[{:status 408}]]}}))))
+    (testing "Should return higher status"
+      (is (= 503
+             (calculate-response-status-code {:hero {:details {:status 200}}
+                                              :villain {:details [[{:status 408}][{:status 503}]]}}))))
     (testing "Should correct status 204 to 200 and return higher status"
-      (is (=
-           200
-           (calculate-response-status-code {:villain {:details {:status 204}}
-                                            :hero {:details {:status 200}}}))))
-
+      (is (= 200
+             (calculate-response-status-code {:villain {:details {:status 204}}
+                                              :hero {:details {:status 200}}}))))
     (testing "Should correct status 0 to 503 and return higher status"
-      (is (=
-           503
-           (calculate-response-status-code {:villain {:details {:status 0}}
-                                            :hero {:details {:status 200}}}))))))
+      (is (= 503
+             (calculate-response-status-code {:villain {:details {:status 0}}
+                                              :hero {:details {:status 200}}}))))
+    (testing "Should correct status nil to 500 and return higher status"
+      (is (= 500
+             (calculate-response-status-code {:villain {:details {}}
+                                              :hero {:details {:status 200}}}))))))
