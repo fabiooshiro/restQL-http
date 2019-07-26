048a3897232138ce2c93b7cc99e160c8c8250e27
Rafael Cupello <rafacupello@gmail.com>
2019-07-26 18:37:03 -0300

    fix(multiplexed-calls): ignore errors when requested (#152)

--- src/clojure/restql/http/query/calculate_response_status_code.clj
+++ src/clojure/restql/http/query/calculate_response_status_code.clj
@@ -1,29 +1,5 @@
 (ns restql.http.query.calculate-response-status-code)
 
-(defn- not-error-ignored? [resource-result]
-  (-> resource-result
-      :details
-      :metadata
-      :ignore-errors
-      (= "ignore")
-      (not)))
-
-(defn- not-error-ignoreds [resource-results]
-  (filter not-error-ignored? resource-results))
-
-(defn- extract-status [details]
-  (cond (sequential? details) (map extract-status details)
-        :else (conj [] (:status details))))
-
-(defn- only-resource-status [resource-result]
-  (->> resource-result
-       (map :details)
-       (map extract-status)
-       (flatten)))
-
-(defn- fix-status [statuses]
-  (replace {nil 500 0 503 204 200} statuses))
-
 (defn- get-max [a b]
   (cond (nil? a) b
         (nil? b) a
@@ -33,12 +9,25 @@
 (defn- higher-value [statuses]
   (reduce get-max 200 statuses))
 
+(defn- fix-status [statuses]
+  (replace {nil 500 0 503 204 200} statuses))
+
+(defn- not-error-ignored? [details]
+  (-> details
+      :metadata
+      :ignore-errors
+      (= "ignore")
+      (not)))
+
 (defn calculate-response-status-code
   "Calculates the response status code of a given result"
   [result]
-  (->> result
-       (vals)
-       (not-error-ignoreds)
-       (only-resource-status)
-       (fix-status)
-       (higher-value)))
+  (->>
+   result
+   (vals)
+   (map :details)
+   (flatten)
+   (filter not-error-ignored?)
+   (map :status)
+   (fix-status)
+   (higher-value)))
--- test/clojure/restql/http/query/status_code_test.clj
+++ test/clojure/restql/http/query/status_code_test.clj
@@ -5,9 +5,9 @@
 (deftest ignoring-errors-when-flagged
   (let [not-error-ignored? #'status-code/not-error-ignored?]
     (testing "Should return true for ignore"
-      (is (false? (not-error-ignored? {:details {:metadata {:ignore-errors "ignore"}}}))))
+      (is (false? (not-error-ignored? {:metadata {:ignore-errors "ignore"}}))))
     (testing "Should return false if no metadata provided"
-      (is (true? (not-error-ignored? {:details {}}))))))
+      (is (true? (not-error-ignored? {}))))))
 
 (deftest returning-higher-value
   (let [higher-value #'status-code/higher-value]
@@ -40,7 +40,7 @@
     (testing "Should return higher status"
       (is (= 503
              (calculate-response-status-code {:hero {:details {:status 200}}
-                                              :villain {:details [[{:status 408}][{:status 503}]]}}))))
+                                              :villain {:details [[{:status 408}] [{:status 503}]]}}))))
     (testing "Should correct status 204 to 200 and return higher status"
       (is (= 200
              (calculate-response-status-code {:villain {:details {:status 204}}
@@ -52,4 +52,17 @@
     (testing "Should correct status nil to 500 and return higher status"
       (is (= 500
              (calculate-response-status-code {:villain {:details {}}
-                                              :hero {:details {:status 200}}}))))))
+                                              :hero {:details {:status 200}}}))))
+    (testing "Should ignore errors"
+      (is (= 200
+             (calculate-response-status-code {:hero {:details {:status 200}}
+                                              :villain {:details {:metadata {:ignore-errors "ignore"}
+                                                                  :status 404}}})))
+      (is (= 200
+             (calculate-response-status-code {:hero {:details {:status 200}}
+                                              :villain {:details [[{:metadata {:ignore-errors "ignore"} :status 408}]
+                                                                  [{:metadata {:ignore-errors "ignore"} :status 503}]]}})))
+      (is (= 200
+             (calculate-response-status-code {:hero {:details {:metadata {:ignore-errors "ignore"} :status 503}}
+                                              :villain {:details [[{:metadata {:ignore-errors "ignore"} :status 200}]
+                                                                  [{:metadata {:ignore-errors "ignore"} :status 503}]]}}))))))
