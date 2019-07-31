e26160e58e11f5fa0265632277e7c38ad8e1b2c0
Rafael Cupello <rafacupello@gmail.com>
2019-07-31 17:18:10 -0300

    bump version to 3.3.0 (#155)

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject restql-http "v3.2.2" :description "restQL HTTP"
+(defproject restql-http "v3.3.0" :description "restQL HTTP"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url  "http://restql.b2w.io"}
